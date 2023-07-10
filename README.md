967a1dc6661600e56f35a57be0add361cb63c28c
Krishna Addepalli <coolkrishna31@gmail.com>
2023-07-10 18:26:57 +0200

    Able to launch Qt Window now

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rust_ffi_example
+
+For QT, export the environment variable QT_PLUGIN_PATH.
+Eg. on Windows: "D:\\Qt\\6.5.1\\msvc2019_64\plugins"
--- rest_client/gui/main.cpp
+++ rest_client/gui/main.cpp
@@ -1,9 +1,7 @@
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QApplication>
-#include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World";
     QApplication app(argc, argv);
 
     QPushButton button("Hello World");
