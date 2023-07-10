97159f6c02a091f93132492f92eebbeaa2e71742
Krishna Addepalli <coolkrishna31@gmail.com>
2023-07-10 17:15:14 +0200

    Added correct options to get Qt to compile with CMake

--- rest_client/gui/main.cpp
+++ rest_client/gui/main.cpp
@@ -1,7 +1,9 @@
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QApplication>
+#include <iostream>
 
 int main(int argc, char** argv) {
+    std::cout << "Hello World";
     QApplication app(argc, argv);
 
     QPushButton button("Hello World");
