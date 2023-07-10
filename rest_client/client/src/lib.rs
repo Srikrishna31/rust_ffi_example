96cade5542d800ef5e49ddf2ccad6a2119f923f7
Krishna Addepalli <coolkrishna31@gmail.com>
2023-07-10 22:51:54 +0200

    WIP to implement a client library

--- rest_client/client/src/lib.rs
+++ rest_client/client/src/lib.rs
@@ -1,4 +1,49 @@
+pub mod errors;
+mod request;
+mod response;
+pub mod utils;
+
+extern crate chrono;
+extern crate cookie;
+#[macro_use]
+extern crate error_chain;
+extern crate fern;
+extern crate libc;
+#[macro_use]
+extern crate log;
+extern crate reqwest;
+
+use response::Response;
+use request::Request;
+use reqwest::Client;
+use crate::errors::ResultExt;
+use crate::errors::Error;
+
 #[no_mangle]
 pub extern "C" fn hello_world() {
     println!("Hello World");
+}
+
+/// Send a `Request`
+pub fn send_request(req: &Request) -> Result<Response, Error> {
+    info!("Sending a GET request to {}", req.destination);
+    if log_enabled!(::log::Level::Debug) {
+        debug!("Sending {} Headers", req.headers.len());
+        for header in req.headers.iter() {
+            debug!("\t{} = {}", header.name(), header.value_string());
+        }
+        for cookie in req.cookies.iter() {
+            debug!("\t{} = {}", cookie.name(), cookie.value());
+        }
+        trace!("{:#?}", req);
+    }
+
+    let client = Client::builder()
+        .build()
+        .chain_err(|| "The native TLS backend couldn't be initialized")?;
+
+    client
+        .execute(req.to_reqwest())
+        .chain_err(|| "The request failed")
+        .and_then(|r| Response::from_reqwest(r))
 }
---
+++ rest_client/client/src/utils.rs
@@ -0,0 +1,43 @@
+use std::sync::Once;
+use fern;
+use log::LevelFilter;
+use chrono::Local;
+
+use crate::errors::*;
+
+/// Initialize the global logger and log to `rest_client.log`.
+///
+/// Note that this is an idempotent function, so you can call it as many times as you want and logging
+/// will only be initialized the first time.
+#[no_mangle]
+pub extern "C" fn initialize_logging() {
+    static INITIALIZE: Once = Once::new();
+    INITIALIZE.call_once(|| {
+        fern::Dispatch::new()
+            .format(|out, message, record| {
+                let loc = record.location();
+
+                out.finish(format_args!(
+                    "{} {:7} ({}#{}): {}{}",
+                    Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                    record.level(),
+                    loc.module_path(),
+                    loc.line(),
+                    message,
+                    if cfg!(windows) {"\r"} else {""}
+                ))
+            })
+            .level(LevelFilter::Debug)
+            .chain(fern::log_file("rest_client.log").unwrap())
+            .apply()
+            .unwrap();
+    });
+}
+
+pub fn backtrace(e: &Error) {
+    error!("Error: {}", e);
+
+    for cause in e.iter().skip(1) {
+        warn!("\tCaused By: {}", cause);
+    }
+}
